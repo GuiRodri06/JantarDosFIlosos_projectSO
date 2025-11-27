ad2b439564ed8520e63497796a36dfc49854f517
Guilherme Rodrigues <guilhermeaz.rodrigues@gmail.com>
2025-11-27 20:28:01 +0000

    Iniciando a classe dos Philosopher

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Insira quantos filosofos vao comer: ");
+        int numPhi = scanner.nextInt();
+
+        List<Philosopher> philosopherList = new ArrayList<>();
+        for (int i = 0; i <= numPhi; i++) {
+            Philosopher philosopher = new Philosopher(i); // ADICIONAR OS OUTROS PARAMETROS DO CONSTRUTOR
+            philosopherList.add(philosopher);
+        }
+
+
+        scanner.close();
+
     }
 }
---
+++ src/Philosopher.java
@@ -0,0 +1,26 @@
+public class Philosopher {
+
+    private Integer id;
+    private Garfo rightFork;
+    private Garfo leftFork;
+
+    public Philosopher(Integer id) {
+        this.id = id;
+    }
+
+    public Philosopher(Integer id, Garfo rightFork, Garfo leftFork) {
+        this.id = id;
+        this.rightFork = rightFork;
+        this.leftFork = leftFork;
+    }
+
+    public void eat(Integer id) {
+        System.out.println("O filosofo " + id + "estas a comer");
+    }
+
+    public void think(Integer id) {
+        System.out.println("O filosofo " + id + "esta a pensar");
+    }
+
+
+}
