e605837696eec4278b41581b5acc726c9fbbb420
EmanuelJunqueira <mansojunqueira@gmail.com>
2025-11-28 09:11:52 +0000

    Adição da classe garfo

--- src/Garfo.java
+++ src/Garfo.java
@@ -1,3 +1,38 @@
 public class Garfo {
-   
+    
+    private final int id;
+    private Philosopher ocupante;
+
+    public Garfo(int id) {
+        this.id = id;
+        this.ocupante = null;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public synchronized int getOcupanteId() {
+        return ( ocupante == null ? -1 : ocupante.getId());
+    }
+
+    public synchronized void segurar (Philosopher f) throws InterruptedException {
+        if (f == null) {
+            throw new IllegalArgumentException("");
+        }
+    }
+
+    while (ocupante != null) {
+        wait();
+    }
+
+    ocupante = f;
+    }
+
+    public synchronized void largar (Philosopher f) {
+        if (ocupante == f) {
+            ocupante = null;
+            notifyAll();
+        }
+    }
 }
