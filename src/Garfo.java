bdcbc4fc9470b7eb67d1c0972e52a5b901a792d6
EmanuelJunqueira <mansojunqueira@gmail.com>
2025-12-08 20:09:57 +0000

    Nova garfo.java e adiciçao do (setStatus)

--- src/Garfo.java
+++ src/Garfo.java
@@ -1,55 +1,52 @@
+import java.util.concurrent.Semaphore;
+
 public class Garfo {
-    
-    private final int id;
-    private Philosopher ocupante;
+   
+    private final Semaphore mutex;
+    private final Semaphore[] semaforosGarfos; 
 
-    public Garfo(int id) {
-        this.id = id;
-        this.ocupante = null;
-    }
+    public Garfo (int n) {
+        mutex = new Semaphore(1); //Apenas um filosofo pode acessar a seçao critica de cada vez
+        semaforosGarfos = new Semaphore[n];
 
-    public int getId() {
-        return id;
-    }
+        //Inicializa os semaforos dos garfos como livres (1)
+        for (int i = 0; i < n; i++) {
+            semaforosGarfos[i] = new Semaphore(1); //cada garfo começa disponivel
 
-    /**
-     * Retorna o Id do filosofo qu esta com o garfo se estiver livre retorna -1
-     * @return ID do ocupante ou -1 se o garfo estiver livre
-     */
-
-    public synchronized int getOcupanteId() {
-        return ocupante == null ? -1 : ocupante.getId();
-    }
-
-
-    /**
-     * Metodo que permite um filosofo segurar o garfo 
-     * A thread so prossegue quando o garfo estiver livre
-     * @throws InterruptedException se a thread for interrompida enquanto espera
-     */
-
-    public synchronized void segurar (Philosopher f) throws InterruptedException {
-        if (f == null) {
-            throw new IllegalArgumentException("O filósofo não pode ser nulo.");
         }
-
-    // Espera enquanto o garfo estiver ocupado
-    while (ocupante != null) {
-        wait();
     }
 
-    ocupante = f;
+    //Metodo que tenta pegar os dois garfos
+    public void pegar(Philosopher f) throws InterruptedException {
+        
+        int id = f.getId();
+        int esquerdo = id; 
+        int direito = (id +1) % semaforosGarfos.length; // (id + 1, com loop)
+
+        //Seção crítica para pegar os garfos
+        mutex.acquire(); //Adquire o mutex para acesso exclusivo
+
+        semaforosGarfos[esquerdo].acquire(); 
+        semaforosGarfos[direito].acquire(); 
+
+        mutex.release(); //Libera o mutex após pegar os garfos
+
+        System.out.println("O filosofo " + (f.getId() + 1) + " pegou os garfos " + esquerdo + " e " + direito);
+        f.setStatus(1);
+
     }
 
+    public void liberar (Philosopher f) {
 
-    /*
-    *Libera o garfo caso o filosofo informado seja realmente aquele que ta com o garfo ~
-    * depois de liberar o notifyAll e utilizado para evitar starvation
-    */
-    public synchronized void largar (Philosopher f) {
-        if (ocupante == f) { // Apenas libera o garfo se o filósofo for realmente o ocupante.
-            ocupante = null;
-            notifyAll();  // Notifica todas as threads que estão possivelmente esperando.
-        }
+        int id = f.getId();
+        int esquerda = id; 
+        int direita = (id + 1) % semaforosGarfos.length;
+
+        //Soltar os garfos
+
+        semaforosGarfos[esquerda].release(); 
+        semaforosGarfos[direita].release();
+
+        System.out.println("O filosofo " + (f.getId() + 1) + " soltou os garfos " + esquerda + " e " + direita);
     }
 }
--- src/Philosopher.java
+++ src/Philosopher.java
@@ -3,29 +3,45 @@ public class Philosopher implements Runnable {
     private Integer id;
     private Garfo rightFork;
     private Garfo leftFork;
+    private int status;
 
     public Philosopher(Integer id) {
         this.id = id;
+        this.status = 0; //Estado inicial
     }
 
     public Philosopher(Integer id, Garfo rightFork, Garfo leftFork) {
         this.id = id;
         this.rightFork = rightFork;
         this.leftFork = leftFork;
+
+        this.status = 0; //Estado inicial
     }
 
     public void eat(Integer id) throws InterruptedException {
+        setStatus(1);// Estado = comer
+
         System.out.println("O filosofo " + id + "estas a comer");
         Thread.sleep(1000);
     }
 
     public void think(Integer id) throws InterruptedException {
+        setStatus(0); // Estado = pensar
+
         System.out.println("O filosofo " + id + "esta a pensar");
         Thread.sleep(1000);
     }
 
     public Integer getId() {
         return id;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public int getStatus() { // controlo/debug
+        return status;
     }
 
 
